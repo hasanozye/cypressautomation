b2554a2dd05290893acbc0013cda071d88e1f885
Hasan Özyer <hasanozyer06@gmail.com>
2023-12-27 16:59:32 +0300

    Different File Uploads class is created for testing and learning

---
+++ cypress/e2e/FileUpload.cy.js
@@ -0,0 +1,52 @@
+
+
+describe("File Uploads", ()=>{
+
+    it("Single File Upload", ()=>{
+
+
+
+    });
+
+
+
+    it("File Upload - Rename", ()=>{
+
+
+
+
+
+    });
+
+
+    it("File upload - Drag and Drop", ()=>{
+
+
+
+
+
+    });
+
+
+
+    it("Multiple files Upload", ()=>{
+
+
+
+
+
+    });
+
+
+
+    it("File upload -Shadow Dom", ()=>{
+
+
+
+
+    });
+
+
+    
+
+})
