00787699c75634be1e4f91d531f15b11832cd907
Hasan Özyer <hasanozyer06@gmail.com>
2023-12-28 20:38:37 +0300

    Learning hooks and tags initial class on cypress

---
+++ cypress/e2e/HooksAndTags.cy.js
@@ -0,0 +1,33 @@
+// before
+// after
+// beforeEach
+// afterEach
+
+describe("MyTestSuite", ()=>{
+
+
+
+
+
+
+
+    it("search",()=>{
+
+
+
+    });
+
+    
+    it("advanced search", ()=>{
+
+
+
+    });
+
+
+    it("listing products", ()=>{
+
+
+    });
+
+});
