8f6ad0a78de351e4af689898046da84429927ab2
Hasan Özyer <hasanozyer06@gmail.com>
2023-12-28 21:12:07 +0300

    This commit introduces hooks (before, after, beforeEach, afterEach) and test cases for the "MyTestSuite" in Cypress. The hooks are used to log actions such as launching the app, closing the app, logging in, and logging out. Additionally, test cases for searching, advanced searching (skipped), and listing products are included.

--- cypress/e2e/HooksAndTags.cy.js
+++ cypress/e2e/HooksAndTags.cy.js
@@ -1,3 +1,6 @@
+//Hooks
+
+
 // before
 // after
 // beforeEach
@@ -5,27 +8,56 @@
 
 describe("MyTestSuite", ()=>{
 
+    before(()=>{
+
+        cy.log("****** Launch App ******");
 
 
+    });
+
+    after(()=>{
+
+        cy.log("****** Close App *********")
 
 
+    });
+
+
+    beforeEach(()=>{
+
+        cy.log("********* Login");
+
+
+    });
+
+
+    afterEach(()=>{
+
+        cy.log("******** Logout");
+
+
+    });
 
 
     it("search",()=>{
+
+        cy.log("*****   searching  ******");
 
 
 
     });
 
     
-    it("advanced search", ()=>{
+    it.skip("advanced search", ()=>{
 
-
+        cy.log("*****   advanced searching  ******");
 
     });
 
 
-    it("listing products", ()=>{
+    it.only("listing products", ()=>{
+
+        cy.log("*****   Listing Products  ******");
 
 
     });
