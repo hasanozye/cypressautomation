60c6cdcfb4d6cf588145ec8d13eb5b2bf2b81dac
Hasan Özyer <hasanozyer06@gmail.com>
2023-12-30 13:23:19 +0300

    initialized a CustomCommands class for learning.

---
+++ cypress/e2e/CustomCommands.cy.js
@@ -0,0 +1,35 @@
+//click on link using label
+// over writing existing contains() command
+// re-usable custom command
+
+
+describe('Custom commands', ()=>{
+
+
+    it("handling links", ()=>{
+
+
+
+
+    })
+
+
+    it("overwriting existing command", ()=>{
+
+
+
+
+
+    })
+
+
+    it ("LOgin Command", ()=>{
+
+
+
+        
+    })
+
+
+
+})
