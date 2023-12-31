5fdf2a235a6f4d650f4c6c506603721854331261
Hasan Özyer <hasanozyer06@gmail.com>
2023-12-31 21:19:25 +0300

    Added custom commands for handling links and login in Cypress tests

--- cypress/e2e/CustomCommands.cy.js
+++ cypress/e2e/CustomCommands.cy.js
@@ -1,5 +1,5 @@
 //click on link using label
-// over writing existing contains() command
+// overwriting existing contains() command
 // re-usable custom command
 
 
@@ -8,26 +8,41 @@ describe('Custom commands', ()=>{
 
     it("handling links", ()=>{
 
+        cy.visit("https://demo.nopcommerce.com/");
 
-
-
-    })
-
-
-    it("overwriting existing command", ()=>{
-
-
-
-
-
-    })
-
-
-    it ("LOgin Command", ()=>{
-
-
-
+        //direct -without using custom command
+            //cy.get(':nth-child(2) > .product-item > .details > .product-title > a').click();
         
+        //using custom command
+        cy.clickLink('Apple MacBook Pro 13-inch');
+        cy.get(".product-name h1").should('contain.text','Apple');
+
+
+
+    })
+
+
+    it.skip("overwriting existing command", ()=>{
+
+        //cy.clickLink("APPLE MacBook Pro 13-inch");
+        //cy.get(".product-name h1").should('contain.text','Apple');        
+
+
+
+
+
+    })
+
+
+    it("Login Command", ()=>{
+
+        cy.visit("https://demo.nopcommerce.com/");
+
+        cy.get(".ico-login").click();
+        cy.login("ehliyethasan@gmail.com","Hasan123");
+        cy.get(".ico-logout").should('have.text','Log out');
+
+
     })
 
 
