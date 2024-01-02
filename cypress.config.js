3953e7a6e6519beb80a08e7dc3771df6b83f151f
Hasan Özyer <hasanozyer06@gmail.com>
2024-01-02 22:45:18 +0300

    added mochawesome plugin for cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   video: true,
+  reporter: 'cypress-mochawesome-reporter',  //for html reports
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      screenshotOnRunFailure=true;
+      require('cypress-mochawesome-reporter/plugin')(on)  //for html reports
       
     },
   },
