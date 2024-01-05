715c82799c171bfc0c445fcace96ba42ecc5dea8
Hasan Özyer <hasanozyer06@gmail.com>
2024-01-05 23:26:52 +0300

    added and well-documented the previous code.

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -1,18 +1,25 @@
-
+/**
+ * Cypress test suite for interacting with elements using CSS locators on the OpenCart website.
+ */
 describe('CSSLocators', () =>{
 
-it('csslocators', () =>{
+    /**
+     * Cypress test for searching a product ('iMac') using a CSS locator for the search input field.
+     */
+    it('csslocators', () =>{
 
-    cy.visit('https://opencart.abstracta.us')
+        // Visit the OpenCart website
+        cy.visit('https://opencart.abstracta.us')
 
-    //cy.get("input[name='search']").type("iMac") // id      tag is optional
+        // Type 'iMac' in the search input field using a CSS locator
+        cy.get("input.form-control[name='search']").type("iMac")
 
-    cy.get("input.form-control[name='search']").type("iMac")
+        // Click the search button using a CSS locator
+        cy.get("span.input-group-btn").click()
 
-    cy.get("span.input-group-btn").click()
+        // Assert that the product 'iMac' is present in the search results using a CSS locator
+        cy.get("h4").contains("iMac")  // Assertion
 
-    cy.get("h4").contains("iMac")  //Assertion
-
-})
+    })
 
 })
